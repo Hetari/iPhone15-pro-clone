cceac6e902a19f4a9049f94e3bdac5ba7e26f9bf
Ebraheem Alhetari <hetari4py@gmail.com>
2024-05-26 00:08:12 +0300

    refactor the utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,49 +1,48 @@
-import hero from '/assets/images/hero.webp';
+import heroImg from '/assets/images/hero.webp';
 
-export const heroImg = hero;
+import heroVideo from '/assets/videos/hero.webm';
+import smallHeroVideo from '/assets/videos/smallHero.webm';
+import highlightFirstVideo from '/assets/videos/highlight-first.webm';
+import highlightSecondVideo from '/assets/videos/hightlight-third.webm';
+import highlightThirdVideo from '/assets/videos/hightlight-sec.webm';
+import highlightFourthVideo from '/assets/videos/hightlight-fourth.webm';
+import exploreVideo from '/assets/videos/explore.webm';
+import frameVideo from '/assets/videos/frame.webm';
 
-import hmv from '/assets/videos/hero.webm';
-import smallmv from '/assets/videos/smallHero.webm';
-import highlightFirstmv from '/assets/videos/highlight-first.webm';
-import highlightSectmv from '/assets/videos/hightlight-third.webm';
-import highlightThirdmv from '/assets/videos/hightlight-sec.webm';
-import highlightFourthmv from '/assets/videos/hightlight-fourth.webm';
-import exploremv from '/assets/videos/explore.webm';
-import framemv from '/assets/videos/frame.webm';
+import appleImg from '/assets/images/apple.svg';
+import searchImg from '/assets/images/search.svg';
+import bagImg from '/assets/images/bag.svg';
+import watchImg from '/assets/images/watch.svg';
+import rightImg from '/assets/images/right.svg';
+import replayImg from '/assets/images/replay.svg';
+import playImg from '/assets/images/play.svg';
+import pauseImg from '/assets/images/pause.svg';
 
-import apple from '/assets/images/apple.svg';
-import search from '/assets/images/search.svg';
-import bag from '/assets/images/bag.svg';
-import watch from '/assets/images/watch.svg';
-import right from '/assets/images/right.svg';
-import replay from '/assets/images/replay.svg';
-import play from '/assets/images/play.svg';
-import pause from '/assets/images/pause.svg';
+import explore1Img from '/assets/images/explore1.webp';
+import explore2Img from '/assets/images/explore2.webp';
+import chipImg from '/assets/images/chip.webp';
+import frameImg from '/assets/images/frame.png';
 
-import explore1 from '/assets/images/explore1.webp';
-import explore2 from '/assets/images/explore2.webp';
-import chip from '/assets/images/chip.webp';
-import frame from '/assets/images/frame.png';
-
-export const heroVideo = hmv;
-export const smallHeroVideo = smallmv;
-export const highlightFirstVideo = highlightFirstmv;
-export const highlightSecondVideo = highlightSectmv;
-export const highlightThirdVideo = highlightThirdmv;
-export const highlightFourthVideo = highlightFourthmv;
-export const exploreVideo = exploremv;
-export const frameVideo = framemv;
-
-export const appleImg = apple;
-export const searchImg = search;
-export const bagImg = bag;
-export const watchImg = watch;
-export const rightImg = right;
-export const replayImg = replay;
-export const playImg = play;
-export const pauseImg = pause;
-
-export const explore1Img = explore1;
-export const explore2Img = explore2;
-export const chipImg = chip;
-export const frameImg = frame;
+export {
+  heroImg,
+  heroVideo,
+  smallHeroVideo,
+  highlightFirstVideo,
+  highlightSecondVideo,
+  highlightThirdVideo,
+  highlightFourthVideo,
+  exploreVideo,
+  frameVideo,
+  appleImg,
+  searchImg,
+  bagImg,
+  watchImg,
+  rightImg,
+  replayImg,
+  playImg,
+  pauseImg,
+  explore1Img,
+  explore2Img,
+  chipImg,
+  frameImg,
+};
